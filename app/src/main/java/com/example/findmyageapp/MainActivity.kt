1020f92a1fe2e118b0225c7080b13ea20ac74a52
saugat.john09 <saugat.john09@gmail.com>
2020-03-01 20:09:50 +0545

    Simple find my age app built.

---
+++ app/src/main/java/com/example/findmyageapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.findmyageapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnGetAge.setOnClickListener {
+            // Add code to run when button click
+            buttonGetAgeClick()
+        }
+
+    }
+
+    fun buttonGetAgeClick() {
+        val userDOB = Integer.parseInt(editTextDOB.text.toString())
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        if (userDOB > currentYear) {
+            Toast.makeText(this, "Please enter your valid DOB", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        val userAgeInYears = currentYear - userDOB
+        tvAge.text = "Your age is $userAgeInYears Years"
+    }
+}
